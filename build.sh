78ce6488f31c89219db87f0fcaf1d52a5bcfff80
Nate <nate.bever@termalabs.com>
2018-06-18 11:25:38 -0600

    pathing the ditac compiler

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-ditac -images images -p xsl-resources-directory resources -f html -p chain-topics yes -p chain-pages both dist/help.html src/main.ditamap
+./ditac-3_2_5/bin/ditac -images images -p xsl-resources-directory resources -f html -p chain-topics yes -p chain-pages both dist/help.html src/main.ditamap
